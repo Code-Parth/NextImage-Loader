dd94126c09a5baf0f9e3ae4d73ae21ff3b414bc9
Parth Parmar <softsparths@gmail.com>
2024-07-03 17:12:55 +0530

    feat: Add lazy loading for images using the `next/image` component

---
+++ components/common/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="w-full border-t border-white/50 border-dashed rounded-lg p-4">
+            <div className="w-full max-w-[90vw] mx-auto">
+                <h1 className="text-xl font-semibold">NextImage-Loader</h1>
+            </div>
+        </footer>
+    );
+}
---
+++ components/common/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="w-full border-b border-white/50 border-dashed rounded-lg p-4">
+            <div className="w-full max-w-[90vw] mx-auto">
+                <h1 className="font-semibold text-4xl">NextImage-Loader</h1>
+            </div>
+        </header>
+    );
+}
